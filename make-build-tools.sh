e6d2739b4f67503ba69603d000d7a392d1fef9b7
Peter Brightwell <peter.brightwell@bbc.co.uk>
2019-11-08 14:47:37 +0000

    Release now tags/v1.0.x not _spec

--- make-build-tools.sh
+++ make-build-tools.sh
@@ -3,11 +3,4 @@
 set -o errexit
 
 git clone https://${GITHUB_TOKEN:+${GITHUB_TOKEN}@}github.com/AMWA-TV/nmos-doc-build-scripts .scripts
-git clone https://${GITHUB_TOKEN:+${GITHUB_TOKEN}@}github.com/AMWA-TV/nmos-doc-layouts .layouts
-rm -rf _layouts assets
-mv .layouts/_layouts .
-mv .layouts/assets .
-rm -rf .layouts
-git clone https://${GITHUB_TOKEN:+${GITHUB_TOKEN}@}github.com/AMWA-TV/raml2html-nmos-theme
-yarn install
-sudo pip3 install jsonref pathlib
+.scripts/install-dependencies.sh
